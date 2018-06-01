eb30063b5450b573474ed831ca484afab53cf3d2
maxotar <maxotar@users.noreply.github.com>
2018-06-01 15:11:09 -0700

    Finish LinkedList and tests.

--- linkedList.py
+++ linkedList.py
@@ -23,36 +23,33 @@ class LinkedList:
 
     def search(self, item):
         cur = self.head
-        found = False
-        while cur is not None and not found:
+        while cur is not None:
             if cur.getData() == item:
-                found = True
+                return True
             else:
                 cur = cur.getNext()
-        return found
+        return False
 
     def remove(self, item):
         if self.length > 0:
             prev = None
             cur = self.head
-            found = False
-            while not found:
+            while cur is not None:
                 if cur.getData() == item:
-                    found = True
+                    if self.length == 1:
+                        self.head = None
+                        self.tail = None
+                    elif prev == None:
+                        self.head = cur.getNext()
+                    else:
+                        prev.setNext(cur.getNext())
+                        if cur.getNext() == None:
+                            self.tail = prev
+                    self.length -= 1
+                    return
                 else:
                     prev = cur
                     cur = cur.getNext()
-            if found:
-                if self.length == 1:
-                    self.head = None
-                    self.tail = None
-                elif prev == None:
-                    self.head = cur.getNext()
-                else:
-                    prev.setNext(cur.getNext())
-                    if cur.getNext() == None:
-                        self.tail = prev
-                self.length -= 1
 
     def append(self, item):
         newNode = Node(item)
@@ -82,71 +79,67 @@ class LinkedList:
             raise ValueError('invalid arguments')
         if not isinstance(pos, int):
             raise TypeError('integer argument expected')
-        if pos > self.length - 1:
-            raise IndexError('index out of range')
-        if pos == 0:
+        if pos < -self.length:
             self.add(item)
-            self.length += 1
-        elif pos == -1:
+            return
+        if pos > self.length - 1:
             self.append(item)
-            self.length += 1
-        else:
-            index = 0
-            prev = None
-            cur = self.head
-            while cur is not None:
-                if index == pos:
-                    newNode = Node(item)
-                    prev.setNext(newNode)
-                    newNode.setNext(cur)
-                    self.length += 1
-                    break
-                else:
-                    prev = cur
-                    cur = cur.getNext()
-                    index += 1
+            return
 
-    def pop(self, pos=-1):
+        if pos < 0:
+            pos = self.length + pos
+
+        index = 0
         prev = None
         cur = self.head
+        while cur is not None:
+            if index == pos:
+                newNode = Node(item)
+                if prev is not None:
+                    prev.setNext(newNode)
+                else:
+                    self.head = newNode
+                newNode.setNext(cur)
+                self.length += 1
+                break
+            else:
+                prev = cur
+                cur = cur.getNext()
+                index += 1
 
-        if cur == None:
+    def pop(self, pos=-1):
+        if self.length == 0:
             raise IndexError('pop from empty list')
         if not isinstance(pos, int):
             raise TypeError('integer argument expected')
-        if pos > self.length - 1:
+        if pos > self.length - 1 or pos < -self.length:
             raise IndexError('index out of range')
-        if pos == -1:
-            while cur.getNext() is not None:
+
+        if pos < 0:
+            pos = self.length + pos
+
+        index = 0
+        prev = None
+        cur = self.head
+        while cur is not None:
+            if index == pos:
+                if prev == None and cur.getNext() == None:
+                    self.head = None
+                    self.tail = None
+                elif cur.getNext() == None:
+                    self.tail = prev
+                    self.tail.setNext(None)
+                elif prev == None:
+                    self.head = cur.getNext()
+                else:
+                    prev.setNext(cur.getNext())
+
+                self.length -= 1
+                return cur.getData()
+            else:
                 prev = cur
                 cur = cur.getNext()
-            if prev == None:
-                self.head = None
-                self.tail = None
-            else:
-                prev.setNext(None)
-                self.tail = prev
-            return cur.getData()
-        else:
-            index = 0
-            while cur.getNext() is not None:
-                if index == pos:
-                    if prev == None and cur.getNext() == None:
-                        self.head = cur.getNext()
-                        self.tail = None
-                    elif cur.getNext() == None:
-                        self.tail = prev
-                        self.tail.setNext(None)
-                    elif prev == None:
-                        self.head = cur.getNext()
-                    else:
-                        prev.setNext(cur.getNext())
-
-                    return cur.getData()
-                else:
-                    prev = cur
-                    cur = cur.getNext()
-                    index += 1
+                index += 1
 
     def toList(self):
         outputList = []
@@ -165,6 +158,3 @@ class LinkedList:
 
     def __str__(self):
         return str(self.toList())
-
-
-# https://stackoverflow.com/questions/22284578/linked-list-in-python-append-index-insert-and-pop-functions-not-sure-with-c?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
--- test/test_linkedList.py
+++ test/test_linkedList.py
@@ -2,67 +2,62 @@ from linkedList import LinkedList
 import pytest
 
 
-def test_isEmpty():
-    mylist = LinkedList()
-    assert mylist.isEmpty()
+class Test_utility:
+    def test_isEmpty(self):
+        mylist = LinkedList()
+        assert mylist.isEmpty()
 
+    def test_search(self):
+        mylist = LinkedList()
+        mylist.add(1)
+        mylist.add(2)
+        mylist.add(3)
+        mylist.add(5)
+        mylist.add(6)
+        assert mylist.search(6)
+        assert not mylist.search(4)
 
-def test_add_search():
-    mylist = LinkedList()
-    mylist.add(1)
-    mylist.add(2)
-    mylist.add(3)
-    mylist.add(5)
-    mylist.add(6)
-    assert mylist.search(6)
-    assert not mylist.search(4)
+    def test_size(self):
+        mylist = LinkedList()
+        mylist.add(1)
+        mylist.add(2)
+        assert mylist.size() == 2
 
+    def test_append(self):
+        mylist = LinkedList()
+        mylist.append(0)
+        assert mylist.toList() == [0]
+        mylist.add(1)
+        assert mylist.toList() == [1, 0]
+        mylist.append(5)
+        assert mylist.toList() == [1, 0, 5]
 
-def test_size():
-    mylist = LinkedList()
-    mylist.add(1)
-    mylist.add(2)
-    assert mylist.size() == 2
+    def test_toList(self):
+        mylist = LinkedList()
+        mylist.add(1)
+        mylist.add(2)
+        mylist.add(3)
+        assert mylist.toList() == [3, 2, 1]
 
+    def test_clear(self):
+        mylist = LinkedList()
+        mylist.add(1)
+        mylist.add(2)
+        mylist.add(3)
+        mylist.clear()
+        assert mylist.size() == 0
+        assert mylist.isEmpty()
+        assert mylist.toList() == []
 
-def test_append():
-    mylist = LinkedList()
-    mylist.append(0)
-    assert mylist.toList() == [0]
-    mylist.add(1)
-    assert mylist.toList() == [1, 0]
-    mylist.append(5)
-    assert mylist.toList() == [1, 0, 5]
-
-
-def test_tolist():
-    mylist = LinkedList()
-    mylist.add(1)
-    mylist.add(2)
-    mylist.add(3)
-    assert mylist.toList() == [3, 2, 1]
-
-
-def test_clear():
-    mylist = LinkedList()
-    mylist.add(1)
-    mylist.add(2)
-    mylist.add(3)
-    mylist.clear()
-    assert mylist.size() == 0
-    assert mylist.isEmpty()
-    assert mylist.toList() == []
-
-
-def test_index():
-    mylist = LinkedList()
-    mylist.add(1)
-    mylist.add(2)
-    mylist.add(3)
-    assert mylist.index(3) == 0
-    assert mylist.index(2) == 1
-    assert mylist.index(1) == 2
-    assert mylist.index(4) == -1
+    def test_index(self):
+        mylist = LinkedList()
+        mylist.add(1)
+        mylist.add(2)
+        mylist.add(3)
+        assert mylist.index(3) == 0
+        assert mylist.index(2) == 1
+        assert mylist.index(1) == 2
+        assert mylist.index(4) == -1
 
 
 class Test_insert:
@@ -76,10 +71,74 @@ class Test_insert:
             mylist = LinkedList()
             mylist.insert(2.2, 1)
 
-    def test_index_out_of_range(self):
-        with pytest.raises(TypeError):
-            mylist = LinkedList()
-            mylist.insert(200, 1)
+    def test_index_low(self):
+        mylist = LinkedList()
+        mylist.add(2)
+        mylist.insert(-100, 1)
+        assert mylist.toList() == [1, 2]
+        assert mylist.size() == 2
+
+    def test_index_high(self):
+        mylist = LinkedList()
+        mylist.add(2)
+        mylist.insert(100, 1)
+        assert mylist.toList() == [2, 1]
+        assert mylist.size() == 2
+
+    def test_insert_front_empty(self):
+        mylist = LinkedList()
+        mylist.insert(0, 1)
+        assert mylist.toList() == [1]
+        assert mylist.size() == 1
+
+    def test_insert_front_one(self):
+        mylist = LinkedList()
+        mylist.add(2)
+        mylist.insert(0, 1)
+        assert mylist.toList() == [1, 2]
+        assert mylist.size() == 2
+
+    def test_insert_front_full_list(self):
+        mylist = LinkedList()
+        mylist.add(2)
+        mylist.add(3)
+        mylist.add(4)
+        mylist.insert(0, 1)
+        assert mylist.toList() == [1, 4, 3, 2]
+        assert mylist.size() == 4
+
+    def test_insert_middle_full_list(self):
+        mylist = LinkedList()
+        mylist.add(1)
+        mylist.add(2)
+        mylist.add(3)
+        mylist.add(4)
+        mylist.insert(2, 17)
+        assert mylist.toList() == [4, 3, 17, 2, 1]
+        assert mylist.size() == 5
+
+    def test_insert_back_empty(self):
+        mylist = LinkedList()
+        mylist.insert(-1, 1)
+        assert mylist.toList() == [1]
+        assert mylist.size() == 1
+
+    def test_insert_back_one(self):
+        mylist = LinkedList()
+        mylist.add(2)
+        mylist.insert(-1, 1)
+        assert mylist.toList() == [1, 2]
+        assert mylist.size() == 2
+
+    def test_insert_back_full_list(self):
+        mylist = LinkedList()
+        mylist.add(2)
+        mylist.add(3)
+        mylist.add(4)
+        mylist.add(5)
+        mylist.insert(-3, 1)
+        assert mylist.toList() == [5, 1, 4, 3, 2]
+        assert mylist.size() == 5
 
 
 class Test_remove:
@@ -143,6 +202,12 @@ class Test_pop:
             mylist.add(2)
             mylist.pop(100)
 
+    def test_index_out_of_range_low(self):
+        with pytest.raises(IndexError):
+            mylist = LinkedList()
+            mylist.add(2)
+            mylist.pop(-100)
+
     def test_one_item_last(self):
         mylist = LinkedList()
         mylist.add(2)
@@ -155,6 +220,7 @@ class Test_pop:
         mylist.add(2)
         mylist.add(3)
         assert mylist.pop(0) == 3
+        assert mylist.size() == 2
 
     def test_many_items_last(self):
         mylist = LinkedList()
@@ -162,3 +228,12 @@ class Test_pop:
         mylist.add(2)
         mylist.add(3)
         assert mylist.pop(-1) == 1
+        assert mylist.size() == 2
+
+    def test_many_items_middle(self):
+        mylist = LinkedList()
+        mylist.add(1)
+        mylist.add(2)
+        mylist.add(3)
+        assert mylist.pop(-2) == 2
+        assert mylist.size() == 2
