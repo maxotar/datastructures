ab8a80e97a89da9b9103d5819b2c3a1956868b5e
maxotar <maxotar@users.noreply.github.com>
2018-06-08 13:33:46 -0700

    Finish hash table open addressing.

--- datastructures/hashTableOpenAddressing.py
+++ datastructures/hashTableOpenAddressing.py
@@ -1,21 +1,15 @@
 class HashTableOpenAddressing:
-
+    TOMBSTONE = 'RIP'
     MINSIZE = 8
 
     def __init__(self, **initial_values):
-        self._buckets = [None] * self.MINSIZE
+        self._buckets = [() for _ in range(self.MINSIZE)]
 
         for k, v in initial_values.items():
             self.insert(k, v)
 
-    def computeHash(self, key):
-        myhash = hash(key)
-        mymask = len(self._buckets) - 1
-        myid = myhash & mymask
-        return (myhash, myid)
-
     def get(self, key):
-        _, myid = self.computeHash(key)
+        _, myid = self._computeHash(key)
         bucketsChecked = 0
         while self._buckets[myid] and self._buckets[myid][1] != key:
             bucketsChecked += 1
@@ -28,8 +22,9 @@ class HashTableOpenAddressing:
         raise KeyError('key does not exist')
 
     def insert(self, key, value):
-        myhash, myid = self.computeHash(key)
+        self._grow()
 
+        myhash, myid = self._computeHash(key)
         if not self._buckets[myid]:
             self._buckets[myid] = (myhash, key, value)
         else:
@@ -45,6 +40,25 @@ class HashTableOpenAddressing:
                 return
             raise IndexError('table is full')
 
+    def delete(self, key):
+        _, myid = self._computeHash(key)
+        if not self._buckets[myid]:
+            self._buckets[myid] = self.TOMBSTONE
+            self._shrink()
+        else:
+            bucketsChecked = 0
+            while self._buckets[myid] and self._buckets[myid][1] != key:
+                bucketsChecked += 1
+                if bucketsChecked < len(self._buckets):
+                    myid = 0 if myid == len(self._buckets) - 1 else myid + 1
+                else:
+                    break
+            else:  # Run if the while condition EVER hits false
+                self._buckets[myid] = self.TOMBSTONE
+                self._shrink()
+                return
+            raise KeyError('key does not exist')
+
     @property
     def utilization(self):
         try:
@@ -52,13 +66,36 @@ class HashTableOpenAddressing:
         except ZeroDivisionError:
             return 0
 
+    @property
+    def keys(self):
+        items = [bucket[1] for bucket in self._buckets if bucket]
+        return items
+
+    @property
+    def values(self):
+        items = [bucket[2] for bucket in self._buckets if bucket]
+        return items
+
+    def _computeHash(self, key):
+        myhash = hash(key)
+        mymask = len(self._buckets) - 1
+        myid = myhash & mymask
+        return (myhash, myid)
+
+    def _grow(self):
+        if self.utilization >= 0.75:
+            self._resize(len(self._buckets) * 4)
+
+    def _shrink(self):
+        if 0 < self.utilization <= 0.16 and len(self._buckets) > self.MINSIZE:
+            self._resize(len(self._buckets) // 4)
+
     def _resize(self, newsize):
-        self.size = newsize
         old_buckets = self._buckets
-        self._buckets = [None] * newsize
+        self._buckets = [() for _ in range(newsize)]
 
         # Move all the old buckets to new table
-        for bucket in [b for b in old_buckets if b]:
+        for bucket in [b for b in old_buckets if b and b != self.TOMBSTONE]:
             self.insert(bucket[1], bucket[2])
 
     def __setitem__(self, key, val):
@@ -71,8 +108,14 @@ class HashTableOpenAddressing:
         else:
             raise KeyError('key does not exist')
 
+    def __delitem__(self, key):
+        self.delete(key)
+
     def __len__(self):
-        return len([x for x in self._buckets if x])
+        return len([b for b in self._buckets if b and b != self.TOMBSTONE])
+
+    def __str__(self):
+        return str(list(zip(self.keys, self.values)))
 
 # http://kells.tj/blog/2015/04/26/pure-python-hashtable.html
 # http://interactivepython.org/courselib/static/pythonds/SortSearch/Hashing.html
--- tests/test_hashTableOpenAddressing.py
+++ tests/test_hashTableOpenAddressing.py
@@ -56,3 +56,59 @@ def test_utilization():
     assert myht.utilization == 0
     myht['one'] = 'dog'
     assert myht.utilization == 1 / len(myht._buckets)
+
+
+def test_keys():
+    myht = HashTableOpenAddressing()
+    myht['one'] = 'dog'
+    myht['two'] = 'cat'
+    assert 'two' in myht.keys
+    assert 'one' in myht.keys
+
+
+def test_values():
+    myht = HashTableOpenAddressing()
+    myht['one'] = 'dog'
+    myht['two'] = 'cat'
+    assert 'dog' in myht.values
+    assert 'cat' in myht.values
+
+
+def test_delete():
+    with pytest.raises(KeyError):
+        myht = HashTableOpenAddressing()
+        myht['one'] = 'dog'
+        myht['two'] = 'cat'
+        myht.delete('two')
+        myht['two']
+
+
+def test_delete_2():
+    myht = HashTableOpenAddressing()
+    myht['one'] = 'dog'
+    myht['two'] = 'cat'
+    del myht['two']
+    assert 'two' not in myht.keys
+
+
+def test_everything():
+    myht = HashTableOpenAddressing()
+    myht['1'] = 'one'
+    myht['2'] = 'two'
+    myht['3'] = 'three'
+    myht['4'] = 'four'
+    myht['5'] = 'five'
+    myht['6'] = 'six'
+    myht['7'] = 'seven'
+    myht['8'] = 'eight'
+    myht['9'] = 'nine'
+    assert len(myht._buckets) == 32
+    del myht['1']
+    del myht['2']
+    del myht['3']
+    del myht['4']
+    del myht['5']
+    del myht['6']
+    del myht['7']
+    del myht['8']
+    assert len(myht._buckets) == 8
