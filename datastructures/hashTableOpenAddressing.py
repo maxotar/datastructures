14c83edcdbf1d6506c9bdac64714c24c64ad253b
maxotar <maxotar@users.noreply.github.com>
2018-06-08 09:32:01 -0700

    Add functions to hash table open addressing

--- datastructures/hashTableOpenAddressing.py
+++ datastructures/hashTableOpenAddressing.py
@@ -1,7 +1,78 @@
 class HashTableOpenAddressing:
-    def __init__(self):
-        pass
 
+    MINSIZE = 8
+
+    def __init__(self, **initial_values):
+        self._buckets = [None] * self.MINSIZE
+
+        for k, v in initial_values.items():
+            self.insert(k, v)
+
+    def computeHash(self, key):
+        myhash = hash(key)
+        mymask = len(self._buckets) - 1
+        myid = myhash & mymask
+        return (myhash, myid)
+
+    def get(self, key):
+        _, myid = self.computeHash(key)
+        bucketsChecked = 0
+        while self._buckets[myid] and self._buckets[myid][1] != key:
+            bucketsChecked += 1
+            if bucketsChecked < len(self._buckets):
+                myid = 0 if myid == len(self._buckets) - 1 else myid + 1
+            else:
+                break
+        else:  # Run if the while condition EVER hits false
+            return self._buckets[myid][2] if self._buckets[myid] else None
+        raise KeyError('key does not exist')
+
+    def insert(self, key, value):
+        myhash, myid = self.computeHash(key)
+
+        if not self._buckets[myid]:
+            self._buckets[myid] = (myhash, key, value)
+        else:
+            bucketsChecked = 0
+            while self._buckets[myid] and self._buckets[myid][1] != key:
+                bucketsChecked += 1
+                if bucketsChecked < len(self._buckets):
+                    myid = 0 if myid == len(self._buckets) - 1 else myid + 1
+                else:
+                    break
+            else:  # Run if the while condition EVER hits false
+                self._buckets[myid] = (myhash, key, value)
+                return
+            raise IndexError('table is full')
+
+    @property
+    def utilization(self):
+        try:
+            return float(len(self)) / float(len(self._buckets))
+        except ZeroDivisionError:
+            return 0
+
+    def _resize(self, newsize):
+        self.size = newsize
+        old_buckets = self._buckets
+        self._buckets = [None] * newsize
+
+        # Move all the old buckets to new table
+        for bucket in [b for b in old_buckets if b]:
+            self.insert(bucket[1], bucket[2])
+
+    def __setitem__(self, key, val):
+        self.insert(key, val)
+
+    def __getitem__(self, key):
+        val = self.get(key)
+        if val:
+            return val
+        else:
+            raise KeyError('key does not exist')
+
+    def __len__(self):
+        return len([x for x in self._buckets if x])
 
 # http://kells.tj/blog/2015/04/26/pure-python-hashtable.html
 # http://interactivepython.org/courselib/static/pythonds/SortSearch/Hashing.html
--- tests/test_hashTableOpenAddressing.py
+++ tests/test_hashTableOpenAddressing.py
@@ -1,1 +1,58 @@
 from datastructures.hashTableOpenAddressing import HashTableOpenAddressing
+import pytest
+
+
+def test_init():
+    myht = HashTableOpenAddressing()
+    assert len(myht._buckets) == myht.MINSIZE
+
+
+def test_init_some_initial_values():
+    myht = HashTableOpenAddressing(one='dog', two='cat')
+    assert myht.get('one') == 'dog'
+    assert myht.get('two') == 'cat'
+
+
+def test_init_too_many_values():
+    myht = HashTableOpenAddressing(one='dog', two='cat',
+                                   three='horse', four='monkey',
+                                   five='goat', six='waffle',
+                                   seven='strawberry', eight='mouse',
+                                   nine='rat')
+    assert myht.get('one') == 'dog'
+    assert myht.get('two') == 'cat'
+    assert myht.get('nine') == 'rat'
+
+
+def test_insert_one():
+    myht = HashTableOpenAddressing()
+    myht.insert('one', 'dog')
+    assert myht.get('one') == 'dog'
+
+
+def test_invalid_key():
+    with pytest.raises(KeyError):
+        myht = HashTableOpenAddressing()
+        myht['one'] = 'dog'
+        myht['two']
+
+
+def test_bracket_method():
+    myht = HashTableOpenAddressing()
+    myht['one'] = 'dog'
+    assert myht['one'] == 'dog'
+
+
+def test_overwrite():
+    myht = HashTableOpenAddressing()
+    myht['one'] = 'dog'
+    assert myht['one'] == 'dog'
+    myht['one'] = 'cat'
+    assert myht['one'] == 'cat'
+
+
+def test_utilization():
+    myht = HashTableOpenAddressing()
+    assert myht.utilization == 0
+    myht['one'] = 'dog'
+    assert myht.utilization == 1 / len(myht._buckets)
