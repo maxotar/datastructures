7d438231181f38ea6320bad11a9e16c4a4e4c97e
maxotar <maxotar@users.noreply.github.com>
2018-06-12 10:01:29 -0700

    Implement hash table with separate chaining.

--- datastructures/hashTableSeparateChaining.py
+++ datastructures/hashTableSeparateChaining.py
@@ -1,6 +1,98 @@
 class HashTableSeparateChaining:
-    def __init__(self):
-        pass
+    SIZE = 128
 
+    def __init__(self, **initial_values):
+        self._buckets = [None] * self.SIZE
+        for key, value in initial_values.items():
+            self.insert(key, value)
+
+    def insert(self, key, value):
+        _, myid = self._computeHash(key)
+        myList = self._buckets[myid]
+        if myList:
+            myNode = myList.nodeFromKey(key)
+            if myNode:
+                myNode.value = value
+            else:
+                myList.add(key, value)
+        else:
+            myList = KeyValueLinkedList()
+            myList.add(key, value)
+            self._buckets[myid] = myList
+
+    def get(self, key):
+        _, myid = self._computeHash(key)
+        myList = self._buckets[myid]
+        if myList:
+            myNode = myList.nodeFromKey(key)
+            if myNode:
+                return myNode.value
+        raise KeyError('invalid key')
+
+    def delete(self, key):
+        _, myid = self._computeHash(key)
+        myList = self._buckets[myid]
+        if myList:
+            myList.delete(key)
+
+    def _computeHash(self, key):
+        myhash = hash(key)
+        mymask = len(self._buckets) - 1
+        myid = myhash & mymask
+        return (myhash, myid)
+
+    def __setitem__(self, key, value):
+        self.insert(key, value)
+
+    def __getitem__(self, key):
+        value = self.get(key)
+        if value:
+            return value
+        else:
+            raise KeyError('key does not exist')
+
+    def __delitem__(self, key):
+        self.delete(key)
+
+
+class KeyValueNode:
+    def __init__(self, key=None, value=None):
+        self.key = key
+        self.value = value
+        self.next = None
+
+
+class KeyValueLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def add(self, key, value):
+        newNode = KeyValueNode(key, value)
+        newNode.next = self.head
+        self.head = newNode
+
+    def nodeFromKey(self, key):
+        cur = self.head
+        while cur:
+            if cur.key == key:
+                return cur
+            else:
+                cur = cur.next
+        return None
+
+    def delete(self, key):
+        prev = None
+        cur = self.head
+        while cur:
+            if cur.key == key:
+                if prev:
+                    prev.next = cur.next
+                else:
+                    self.head = None
+                return
+            else:
+                prev = cur
+                cur = cur.next
+        raise KeyError('invalid key')
 
 # https://www.interviewbreeze.com/learn/hash-table-implementation/
--- tests/test_hashTableSeparateChaining.py
+++ tests/test_hashTableSeparateChaining.py
@@ -1,1 +1,49 @@
 from datastructures.hashTableSeparateChaining import HashTableSeparateChaining
+import pytest
+
+
+def test_creation():
+    myht = HashTableSeparateChaining(one='dog', two='cat',
+                                     three='horse', four='monkey',
+                                     five='goat', six='waffle',
+                                     seven='strawberry', eight='mouse',
+                                     nine='rat')
+    assert myht.get('one') == 'dog'
+    assert myht.get('two') == 'cat'
+    assert myht.get('nine') == 'rat'
+
+
+def test_insert_one():
+    myht = HashTableSeparateChaining()
+    myht.insert('one', 'dog')
+    assert myht.get('one') == 'dog'
+
+
+def test_invalid_key():
+    with pytest.raises(KeyError):
+        myht = HashTableSeparateChaining()
+        myht['one'] = 'dog'
+        myht['two']
+
+
+def test_bracket_method():
+    myht = HashTableSeparateChaining()
+    myht['one'] = 'dog'
+    assert myht['one'] == 'dog'
+
+
+def test_overwrite():
+    myht = HashTableSeparateChaining()
+    myht['one'] = 'dog'
+    assert myht['one'] == 'dog'
+    myht['one'] = 'cat'
+    assert myht['one'] == 'cat'
+
+
+def test_delete():
+    with pytest.raises(KeyError):
+        myht = HashTableSeparateChaining()
+        myht['one'] = 'dog'
+        myht['two'] = 'cat'
+        myht.delete('two')
+        myht['two']
